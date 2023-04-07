c9fd12f83d6b1f74ac850d35e8f188433717dec1
Jose Sanchez Viloria <josersanvil@gmail.com>
2023-04-07 21:54:31 +0200

    Added quota reset period

--- deploy/chatbot/package/models/subscription_plan.py
+++ deploy/chatbot/package/models/subscription_plan.py
@@ -17,14 +17,17 @@ class SubscriptionPlan(BaseModel):
     """ Description of the subscription plan. """
     description: str
 
-    """Number of sessions allowed per day in the plan"""
+    """Number of sessions allowed per period in the plan, None if unlimited"""
     sessions_quota: Union[int, None]
 
-    """Number of messages allowed per session in the plan"""
+    """Number of messages allowed per session in the plan, None if unlimited"""
     messages_quota: Union[int, None]
 
-    """Number of image requests allowed per session in the plan"""
+    """Number of image requests allowed per session in the plan, None if unlimited"""
     image_generation_quota: Union[int, None]
 
-    """Resolution of the image generated in the plan"""
+    """Resolution of the images generated in the plan"""
     image_resolution_size: Literal["small", "medium", "large"]
+
+    """Period of time in which the quota is reset."""
+    quota_reset_period: Union[Literal["day"], None] = None
